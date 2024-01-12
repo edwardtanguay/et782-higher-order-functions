17c857315bc3c420901253355fbe232e977474fd
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-12 10:14:54 +0100

    frontendEmployees listed

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 import './style.css'
 import employees from './data/employees.json';
 
+// SHOW EMPLOYEES AS ARRAY
 // console.log(employees);
 
-//
-for (const employee of employees) {
-	console.log(`${employee.lastName}, ${employee.firstName}`);
+// LIST OUT EMPLOYEES AS LASTNAME, FIRSTNAME
+// for (const employee of employees) {
+// 	console.log(`${employee.lastName}, ${employee.firstName}`);
+// }
+
+const checkTaxes = (employee) => {
+	// logic that checks a tax database
+	return false;
 }
+
+// USE MAP TO CONVERT employee TO frontendEmployees (firstName, lastName, city)
+const frontendEmployees = employees.map(employee => {
+	const submittedTaxes = checkTaxes(employee);
+	return {
+		firstName: employee.firstName,
+		lastName: employee.lastName,
+		headquarters: employee.address.city,
+		isOpen: false,
+		submittedTaxes
+	};
+});
+
+console.log(frontendEmployees);
 
 document.querySelector('#app').innerHTML = /*html*/ `
 <h1>Higher Order Graphics</h1>
 
-<h2>List employees as last name, first name</h2>
-
+<h2>List employees as last name, first name (headquarters)</h2>
+<p>There are ${frontendEmployees.length} employees:</p>
+<ul>
+	${frontendEmployees.map(m => `<li>${m.lastName}, ${m.firstName} (${m.headquarters})</li>`).join('')}
+</ul>
 `
